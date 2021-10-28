c65d3eef34fb35b995874986fe70e03ae946c099
0xlain <basedlainpilled@gmail.com>
2021-10-28 18:00:43 -0400

    add liquidity to Token class

--- src/app/Tabs/Stats.jsx
+++ src/app/Tabs/Stats.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
+import { useTokens } from "../hooks/useTokens";
 
 export const Stats = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const tokens = useTokens();
+
+  return (
+    <div>
+      {tokens.map((token, i) => (
+        <Typography key={i}>
+          {token.symbol}
+          {"  "}
+          {token.liquidity}
+        </Typography>
+      ))}
+    </div>
+  );
+};
