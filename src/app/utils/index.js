6d9ab6db09dcc68b2054fe9bb74fe664f0032c05
Caranell <lyachek.anton@gmail.com>
2021-11-01 20:40:31 +0300

    Added BigNumber balances formatting

--- src/app/utils/index.js
+++ src/app/utils/index.js
@@ -1,7 +1,27 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { TOKEN_DECIMALS, ROUNDING_DECIMALS } from "../utils/constants";
+
 export const formatNumber = (number) => {
   if (!number || typeof number !== "number") {
     return number;
   }
 
   return number.toLocaleString("en-US", { minimumFractionDigits: 4 });
+};
+
+export const formatUserBalance = (balance) => {
+  if (!balance) {
+    return balance;
+  }
+  const poolCoeff = BigNumber.from(10).pow(TOKEN_DECIMALS);
+
+  const balanceDecimals = balance
+    .mod(poolCoeff)
+    .div(BigNumber.from(10).pow(TOKEN_DECIMALS - ROUNDING_DECIMALS))
+    .toNumber();
+  const balanceInteger = balance.div(poolCoeff).toNumber();
+
+  const userBalance = balanceInteger + balanceDecimals;
+
+  return formatNumber(userBalance);
 };
